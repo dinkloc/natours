dd23f4be6c9666b499c7ecb17f202d4cb66ff071
5thMayy <nguyendinhloc502@gmail.com>
2023-02-19 14:41:12 +0700

    crud api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from the server side!', app: 'natours'});
@@ -20,6 +21,72 @@ app.get('/api/v1/tours', (req, res) => {
         data:  {
             tours: tours
         }
+    })
+})
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail', 
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success', 
+        data:  {    
+            tour
+        }
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail', 
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here>'
+        }
+    })
+})
+
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail', 
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tours: newTour
+            }
+        })
     })
 })
 

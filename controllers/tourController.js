ef36ea2aab739c81f4e31d735974d3a4cf93c130
5thMayy <nguyendinhloc502@gmail.com>
2023-02-24 13:41:51 +0700

    create middleware check id tour and check create body tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,30 @@
 const fs = require('fs')
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+// Middlewrare check id invalid
+
+exports.checkID = (req, res, next, val ) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail', 
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
+
+// Check body before create
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'missing name or price'
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success', 
@@ -16,13 +40,6 @@ exports.getTour = (req, res) => {
     console.log(req.params);
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail', 
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success', 
         data:  {    
@@ -48,13 +65,6 @@ exports.createTour = (req, res) => {
 
 
 exports.updateTour =  (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail', 
-            message: 'Invalid ID'
-        });
-    }
-    
     res.status(200).json({
         status: 'success',
         data: {
@@ -64,13 +74,6 @@ exports.updateTour =  (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail', 
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(204).json({
         status: 'success',
         data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
+// Call middleware
+router.param('id', tourController.checkID );
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post( tourController.checkBody,tourController.createTour);
 
 router
     .route('/:id')

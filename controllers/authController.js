7c2eb1ef4d162f1eeb0f8d3da207e62fcbf9cad6
dinkloc <nguyendinhloc502@gmail.com>
2023-04-22 13:36:05 +0700

    Authentication with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,10 @@
+const { promisify} = require('util')
 const User = require('./../models/userModels');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('./../utils/appError');
+const { access } = require('fs');
+const { decode } = require('punycode');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -41,8 +44,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
+        role: req.body.role,
         password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
+        passwordConfirm: req.body.passwordConfirm,
+        passwordChangedAt: req.body.passwordChangedAt
     });
 
     const token = signToken(newUser._id);
@@ -64,6 +69,7 @@ exports.login = catchAsync(async (req, res, next) => {
     }
     // 2) Check if user exists && password is correct
     const user = await User.findOne({ email }).select('+password');
+    console.log(user);
 
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401));
@@ -78,3 +84,53 @@ exports.login = catchAsync(async (req, res, next) => {
 
 });
   
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (
+        req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+        return next (
+            new AppError('You are not logged in! Please log in to the access', 401)
+        );
+    }
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+
+    const freshUser = await User.findById(decoded.id); 
+    if (!freshUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        );
+    }
+    // 4) Check if user changed password after the token was issued
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again', 401)
+        );
+    }
+    req.user = freshUser;
+    next(); 
+})
+
+exports.restricTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError('You do not have permission to perform this action', 403)
+            );
+        }
+
+        next();
+    };
+};
